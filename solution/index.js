7b78d3b50260a1549cd49ea5d96d33a07de9bd6d
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-06 13:59:48 +0300

    some edits

--- solution/index.js
+++ solution/index.js
@@ -10,27 +10,30 @@ let counter = 0;
 let lowest = 0;
 let highest = 100;
 
-console.log(rasgeleSayi);
 myBtn.addEventListener("click", () => {
     if(myInp.value > lowest && myInp.value < highest){
         counter++
         atmpt.innerText = counter
         if(rasgeleSayi == myInp.value){
-            myMsg.innerText = "Lucky. You won !"
+            myMsg.innerText = `${myInp.value} is right. Lucky. You won !`
         }
         else if(rasgeleSayi > myInp.value){
-            myMsg.innerText = `Enter a number between ${myInp.value} and ${highest}`;
+            myMsg.innerText = `${myInp.value} is wrong. Enter a number between ${myInp.value} and ${highest}`;
             lowest = myInp.value
                
         }
         else if(rasgeleSayi < myInp.value){
-            myMsg.innerText = `Enter a number between ${lowest} and ${myInp.value}`
+            myMsg.innerText = `${myInp.value} is wrong. Enter a number between ${lowest} and ${myInp.value}`
             highest = myInp.value
         }
     }else{
         myMsg.innerText = `Enter a number between 0 and 100`
     }
-})
+});
+
+
+
+ 
 
 
 
